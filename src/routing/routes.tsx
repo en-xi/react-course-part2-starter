7436049bbb9d9c53ce527d64b8e9ef08461012e8
hassan <enxi666freedom@gmail.com>
2025-10-31 00:38:25 +0800

    Exercise - Working with Nested Routes

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./HomePage";
 import Layout from "./Layout";
-import UserDetailPage from "./UserDetailPage";
-import UserListPage from "./UserListPage";
+import UserDetail from "./UserDetail";
+import UsersPage from "./UsersPage";
 
 const routes = createBrowserRouter([
   {
@@ -10,8 +10,12 @@ const routes = createBrowserRouter([
     element: <Layout />,
     children: [
       { index: true, element: <HomePage /> },
-      { path: "users", element: <UserListPage /> },
-      { path: "users/:id", element: <UserDetailPage /> },
+      {
+        path: "users",
+        element: <UsersPage />,
+
+        children: [{ path: ":id", element: <UserDetail /> }],
+      },
     ],
   },
 ]);
